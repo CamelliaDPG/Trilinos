e56f629654d5619b3c4c81723b2e423ce2f43de0
Nate Roberts <nvrober@sandia.gov>
2022-07-29 15:28:56 -0600

    Simplifying further.

--- packages/intrepid2/unit-test/MonolithicExecutable/BasisCardinalityTests.cpp
+++ packages/intrepid2/unit-test/MonolithicExecutable/BasisCardinalityTests.cpp
@@ -11,31 +11,14 @@ namespace
   using BasisBase = typename LineBasis::BasisBase;
   using BasisPtr = Teuchos::RCP<BasisBase>;
 
-  static BasisPtr getHypercubeBasis_HGRAD2(int polyOrder)
-  {
-    using Teuchos::rcp;
-
-    BasisPtr lineBasis = Teuchos::rcp(new LineBasis(polyOrder) );
-    BasisPtr tensorBasis = lineBasis;
-    tensorBasis = Teuchos::rcp(new Basis_TensorBasis<BasisBase>(tensorBasis, lineBasis, FUNCTION_SPACE_HGRAD));
-
-    /*for (int d=1; d<spaceDim; d++)
-    {
-      tensorBasis = Teuchos::rcp(new Basis_TensorBasis<BasisBase>(tensorBasis, lineBasis, FUNCTION_SPACE_HGRAD));
-    }*/
-
-    return tensorBasis;
-  }
-
   TEUCHOS_UNIT_TEST( BasisCardinality, Hypercube )
   {
    const int polyDegree   = 1;
-   auto nodalBasis        = getHypercubeBasis_HGRAD2(polyDegree);
-   // the following succeeds:
-   //auto nodalBasis = getLineBasis<NodalBasisFamily>(FUNCTION_SPACE_HGRAD, polyDegree);
+   BasisPtr lineBasis = Teuchos::rcp(new LineBasis(polyDegree) );
+   BasisPtr tensorBasis = lineBasis;
+   tensorBasis = Teuchos::rcp(new Basis_TensorBasis<BasisBase>(tensorBasis, lineBasis, FUNCTION_SPACE_HGRAD));
    int expectedExtrusionCount = 1;
-   TEST_EQUALITY(expectedExtrusionCount, nodalBasis->getNumTensorialExtrusions());
+   TEST_EQUALITY(expectedExtrusionCount, tensorBasis->getNumTensorialExtrusions());
   }
 
 } // namespace
-
