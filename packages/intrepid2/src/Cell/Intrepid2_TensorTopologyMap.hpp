fb0d49c1567e1ebcd89bf042832425d512b8d022
Nate Roberts <nvrober@sandia.gov>
2022-08-01 14:08:27 -0600

    Reenabling, reworking (slightly) the few lines whose disablement let us compile last.

--- packages/intrepid2/src/Cell/Intrepid2_TensorTopologyMap.hpp
+++ packages/intrepid2/src/Cell/Intrepid2_TensorTopologyMap.hpp
@@ -117,9 +117,9 @@ namespace Intrepid2
         unsigned subcellCount1 = cellTopo1.getSubcellCount(d1);
         for (unsigned subcellOrdinal1=0; subcellOrdinal1<subcellCount1; subcellOrdinal1++)
         {
-          Subcell subcell1 = {d1, subcellOrdinal1};
-//          std::set<unsigned> subcell1Nodes; // set because we don't care about ordering
-//          unsigned nodeCount1 = cellTopo1_.getNodeCount(d1, subcellOrdinal1);
+          std::set<unsigned> subcell1Nodes; // set because we don't care about ordering
+          unsigned nodeCount1 = cellTopo1_.getNodeCount(d1, subcellOrdinal1);
+          Subcell subcell1 {d1, subcellOrdinal1};
 //          // unfortunately, the node count for vertices is given as 0 by ::shards::CellTopology.  This seems like a bug.
 //          if (d1 == 0)
 //          {
