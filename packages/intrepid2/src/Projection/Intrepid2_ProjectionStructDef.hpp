71ad38f7f6ebe1c53a058e37be80c179d1853e5e
Nate Roberts <nvrober@sandia.gov>
2023-11-02 15:51:02 -0600

    Fixed ref to HCURL::PYR basis in Intrepid2_ProjectionStructDef.

--- packages/intrepid2/src/Projection/Intrepid2_ProjectionStructDef.hpp
+++ packages/intrepid2/src/Projection/Intrepid2_ProjectionStructDef.hpp
@@ -602,7 +602,7 @@ void ProjectionStruct<DeviceType,ValueType>::createHDivProjectionStruct(const Ba
   else if(cellTopo.getKey() == shards::getCellTopologyData<shards::Wedge<6> >()->key)
     hcurlBasis = new typename DerivedNodalBasisFamily<HostDeviceType,ValueType,ValueType>::HCURL_WEDGE(cellBasis->getDegree());
   else if(cellTopo.getKey() == shards::getCellTopologyData<shards::Pyramid<5> >()->key)
-    hcurlBasis = new typename DerivedNodalBasisFamily<DeviceType,ValueType,ValueType>::HCURL_PYR(cellBasis->getDegree());
+    hcurlBasis = new typename HierarchicalBasisFamily<DeviceType,ValueType,ValueType>::HCURL_PYR(cellBasis->getDegree());
   else if(cellTopo.getKey() == shards::getCellTopologyData<shards::Quadrilateral<4> >()->key)
     hcurlBasis = new Basis_HGRAD_QUAD_Cn_FEM<HostDeviceType,ValueType,ValueType>(cellBasis->getDegree());
   else if(cellTopo.getKey() == shards::getCellTopologyData<shards::Triangle<3> >()->key)
