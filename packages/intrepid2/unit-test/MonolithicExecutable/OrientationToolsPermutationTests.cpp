cae7bc60d03bb3a7714ae13531d03e2bf2efe4b4
Nate Roberts <nvrober@sandia.gov>
2024-12-12 11:55:17 -0600

    Fixed build issues in OrientationToolsPermutationTests, and started making notes and stubbing out the core, generic test.
    Signed-off-by: Nate Roberts <nvrober@sandia.gov>

--- packages/intrepid2/unit-test/MonolithicExecutable/OrientationToolsPermutationTests.cpp
+++ packages/intrepid2/unit-test/MonolithicExecutable/OrientationToolsPermutationTests.cpp
@@ -20,6 +20,8 @@
 #include "Intrepid2_NodalBasisFamily.hpp"
 #include "Intrepid2_SerendipityBasisFamily.hpp"
 
+#include "Intrepid2_TestUtils.hpp"
+
 #include "Kokkos_Core.hpp"
 
 namespace
@@ -34,8 +36,7 @@ namespace
 
   ordinal_type ortMax(CellTopoPtr cellTopo)
   {
-    cellTopo->
-    auto cellTopologyKey = cellTopo.getBaseKey();
+    auto cellTopologyKey = cellTopo->getKey().first; // key is (shardsKey, numTensorExtrusions)
     switch (cellTopologyKey) {
       case shards::Node::key:             return 0;
       case shards::Line<2>::key:          return 1;
@@ -50,9 +51,38 @@ namespace
   }
 
   void testOrientationsArePermutations(const Intrepid2::CellTopology &cellTopo, BasisPtr<DeviceType,Scalar,Scalar> basis,
-                                     Teuchos::FancyOStream &out, bool &success)
+                                       Teuchos::FancyOStream &out, bool &success)
   {
-    auto maxOrt = ortMax(cellTopo.getSubcell(2,0));
+    // 1. Check that the identity orientation (0's for every edge and face) is a permutation
+    
+    // 2. Iterate through each possible face/edge orientation. For each:
+    //    - set the appropriate orientation for the face or edge in what is otherwise an identity orientation for the cell
+    //    - examine the data structure (the coefficient matrix) corresponding to that face or edge to determine whether it should be a permutation
+    //    - place the orientation into a single-element Kokkos::View suitable for passing to OrientationTools::orientationsArePermutations()
+    //    - verify that OrientationTools::orientationsArePermutations() returns the correct thing
+    //    - store each cell orientation into one of two std::vectors, depending on whether it is a permutation or not.
+    
+    // 3. Place all permutation orientations into a single view; verify orientationsArePermutations() returns true
+    
+    // 4. Place all non-permutation orientations into a single view; verify orientationsArePermutations() returns false
+    
+    // 5. Place all permutation orientations and one non-permutation orientation (if available) into a single View; verify orientationsArePermutations() returns false
+    
+    std::vector<Orientation> permutationOrts, nonPermutationOrts;
+    
+    for (ordinal_type d=1; d<cellTopo.getDimension(); d++)
+    {
+      ordinal_type subcellCount = cellTopo.getSubcellCount(d);
+      auto maxOrt = ortMax(cellTopo.getSubcell(d,0));
+      
+      for (ordinal_type scOrt=0; scOrt<maxOrt; scOrt++)
+      {
+        
+      }
+    }
+    
+    // orientationsArePermutations(const OrientationViewType orts, const BasisType * basis)
+    
   }
   
   TEUCHOS_UNIT_TEST( OrientationTools, OrientationsArePermutations_Tet )
@@ -63,6 +93,6 @@ namespace
     int edgeDim = 1;
     int faceDim = 2;
     
-    cellTopo.getSubcell(<#int scdim#>, <#int scord#>)
+//    cellTopo.getSubcell(<#int scdim#>, <#int scord#>)
   }
 } // namespace
