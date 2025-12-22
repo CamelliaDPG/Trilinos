03a2f0e8bd248b6bdda8d29a0bdf8b44f153353f
Nate Roberts <nvrober@sandia.gov>
2025-12-22 11:11:36 -0600

    OverlappingPartitioner: further revision, to only do the sanity check when localNumRows > 0.
    Signed-off-by: Nate Roberts <nvrober@sandia.gov>

--- packages/ifpack2/src/Ifpack2_OverlappingPartitioner_def.hpp
+++ packages/ifpack2/src/Ifpack2_OverlappingPartitioner_def.hpp
@@ -143,12 +143,14 @@ setParameters (Teuchos::ParameterList& List)
   if (NumLocalParts_ < 0) {
     NumLocalParts_ = Graph_->getLocalNumRows() / (-NumLocalParts_);
   }
-  TEUCHOS_TEST_FOR_EXCEPTION(
-    NumLocalParts_ < 0 ||
-    Teuchos::as<size_t> (NumLocalParts_) > Graph_->getLocalNumRows(),
-    std::runtime_error,
-    "Ifpack2::OverlappingPartitioner::setParameters: "
-    "Invalid NumLocalParts_ = " << NumLocalParts_ << ".");
+  if (Graph_->getLocalNumRows() > 0)
+  {
+    TEUCHOS_TEST_FOR_EXCEPTION(NumLocalParts_ < 0 ||
+      Teuchos::as<size_t> (NumLocalParts_) > Graph_->getLocalNumRows(),
+      std::runtime_error,
+      "Ifpack2::OverlappingPartitioner::setParameters: "
+      "Invalid NumLocalParts_ = " << NumLocalParts_ << ".");
+  }
   if (NumLocalParts_ == 0) {
     NumLocalParts_ = 1;
   }
